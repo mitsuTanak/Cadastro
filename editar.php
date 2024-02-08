f0bc344e123dd8812b7587a63276c5e401d3134b
Mitsuaki Tanaka <130664643+mitsuTanak@users.noreply.github.com>
2024-02-08 15:37:52 -0300

    Correções

--- editar.php
+++ editar.php
@@ -1,57 +1,59 @@
 <?php
 
-
-// Incluir a conexão com o banco de dados
+// Incluir a conexao com o banco de dados
 include_once "conexao.php";
 
 // Receber os dados do formulário via método POST
 $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
-//Validar o formulário
+// validar o formulario
 if (empty($dados['nome'])) {
-
-    $retorna = ['status' => false, 'msg'=> "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo nome!</div>"];
+    // Se o campo nome estiver vazio, retorna mensagem de erro
+    $retorna = ['status' => false, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo nome!</div>"];
 
 } elseif (empty($dados['email'])) {
-
-    $retorna = ['status' => false, 'msg'=> "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo e-mail!</div>"];
+    // Se o campo email estiver vazio, retorna mensagem de erro
+    $retorna = ['status' => false, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo e-mail!</div>"];
 
 } elseif (empty($dados['logradouro'])) {
-
-    $retorna = ['status' => false, 'msg'=> "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo logradouro!</div>"];
+    // Se o campo logradouro estiver vazio, retorna mensagem de erro
+    $retorna = ['status' => false, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo logradouro!</div>"];
 
 } elseif (empty($dados['numero'])) {
-
-    $retorna = ['status' => false, 'msg'=> "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo número!</div>"];
+    // Se o campo número estiver vazio, retorna mensagem de erro
+    $retorna = ['status' => false, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo número!</div>"];
 
 } else {
-
+    // Editar no banco de dados na primeira tabela (usuarios)
     $query_usuario = "UPDATE usuarios SET nome=:nome, email=:email WHERE id=:id";
-
     $edit_usuario = $conn->prepare($query_usuario);
     $edit_usuario->bindParam(':nome', $dados['nome']);
     $edit_usuario->bindParam(':email', $dados['email']);
     $edit_usuario->bindParam(':id', $dados['id']);
 
-    if($edit_usuario->execute()) {
-        $query_endereco = "UPDATE enderecos SET logradouro=:logradouro, numero = :numero WHERE usuario_id = :usuario_id";
+    // Verificar se editou o usuario
+    if ($edit_usuario->execute()) {
 
+        // Editar no banco de dados na segunda tabela (enderecos)
+        $query_endereco = "UPDATE enderecos SET logradouro=:logradouro, numero=:numero WHERE usuario_id=:usuario_id";
         $edit_endereco = $conn->prepare($query_endereco);
         $edit_endereco->bindParam(':logradouro', $dados['logradouro']);
         $edit_endereco->bindParam(':numero', $dados['numero']);
         $edit_endereco->bindParam(':usuario_id', $dados['id']);
 
-        if($edit_usuario->execute()) {
-            $retorna = ['status' => true, 'msg'=> "<div class='alert alert-succesess' role='alert'>Usuário editado com sucesso!</div>"];
-        }else{
-            $retorna = ['status' => false, 'msg'=> "<div class='alert alert-danger' role='alert'> Erro: Usuário não editado com sucesso!</div>"];
+        // Verificar se editou o endereco
+        if ($edit_endereco->execute()) {
+            $retorna = ['status' => true, 'msg' => "<div class='alert alert-success' role='alert'>Usuário editado com sucesso!</div>"];
+
+        } else {
+            // Se a edição do usuário foi bem-sucedida, mas a edição do endereço não foi
+            $retorna = ['status' => false, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Usuário não editado com sucesso!</div>"];
         }
-    }else{
-        $retorna = ['status' => false, 'msg'=> "<div class='alert alert-danger' role='alert'> Erro: Usuário não editado com sucesso!</div>"];
+    } else {
+        // Se a edição do usuário não foi bem-sucedida
+        $retorna = ['status' => false, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Usuário não editado com sucesso!</div>"];
     }
 }
 
-// Retornar o resultado em formato JSON
+// Retorna o resultado em formato JSON
 echo json_encode($retorna);
-
-?>
